ab7a6c7f8b1bfb953b635504f30fd60696b69195
Lizeth Triana <lvtriana@uniboyaca.edu.co>
2025-05-19 00:43:35 -0500

    avancelizeth

--- view/bills/formAdd.php
+++ view/bills/formAdd.php
@@ -10,6 +10,7 @@ include '../../controllers/billsController.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/paginas.css">
     <title>Add Bill</title>
 </head>
 
